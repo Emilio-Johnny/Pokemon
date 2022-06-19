ebbf2c09f19ccfb2f5b1c577b48ba09c9dc573d7
Emilio Benavides <emilio.benavides68@gmail.com>
2022-06-18 20:13:01 -0500

    added header, pokemon-table, footer

--- src/components/main-page/MainPage.tsx
+++ src/components/main-page/MainPage.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const MainPage = () => {
     return (
-       <div>Main Page</div>
-    );
+        <>
+            <div>Main Page</div>
+            <div>header</div>
+            <div>pokemon-table</div>
+            <div>footer</div>
+            );
+        </>)
 };
+
 
 export default MainPage;
